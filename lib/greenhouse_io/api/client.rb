e8a0577ae0f4056e393b50644d9f7709aeb75ef2
James Craft <james.j.craft@gmail.com>
2018-10-25 19:15:10 -0700

    Add email as a permitted option

--- lib/greenhouse_io/api/client.rb
+++ lib/greenhouse_io/api/client.rb
@@ -3,7 +3,7 @@ module GreenhouseIo
     include HTTMultiParty
     include GreenhouseIo::API
 
-    PERMITTED_OPTIONS = [:page, :per_page, :job_id, :created_after]
+    PERMITTED_OPTIONS = [:page, :per_page, :job_id, :created_after, :email]
 
     attr_accessor :api_token, :rate_limit, :rate_limit_remaining, :link
     base_uri 'https://harvest.greenhouse.io/v1'
